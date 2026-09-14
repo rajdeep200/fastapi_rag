1d8eda83cd3b1023c3b1af864fbf4e56b109e59c
Rajdeep Ghosh <grajdeep2000@gmail.com>
2026-09-14 17:47:00 +0530

    Push latest database connection changes

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException, status, Depends
-from app.schemas.user import UserCreate, UserResponse
+from app.schemas.user import UserCreate, UserResponse, UserUpdate
 from app.services import user_service
 from sqlalchemy.orm import Session
 from app.dependencies import get_db
@@ -9,38 +9,37 @@ router = APIRouter(
     tags=["Users"]
 )
 
-@router.get("/")
-def get_users():
-    return [
-        {
-            "id": 1,
-            "name": "Rajdeep",
-            "email": "rajdeep@example.com",
-            "age": 25
-        }
-    ]
+@router.get(
+    "/",
+    response_model=list[UserResponse]
+)
+def get_users(
+    db: Session = Depends(get_db)
+):
+    return user_service.get_users(db)
     
 @router.get("/{user_id}", response_model=UserResponse)
-def get_user(user_id: int):
-    if user_id != 1:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-    
-    return {
-        "id": 1,
-        "name": "Rajdeep",
-        "email": "rajdeep@example.com",
-        "age": 25
-    }
+def get_user(
+    user_id: int,
+    db: Session = Depends(get_db)
+):
+    return user_service.get_user(db, user_id)
     
 @router.post("/", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 def create_use(user: UserCreate, db: Session = Depends(get_db)):
     return user_service.create_user(user, db)
 
-# @router.get("/profile")
-# def get_user_profile(
-#     current_user: dict = Depends(get_current_user)
-# ):
-#     return {
-#         "message": "Profile fetched successfully",
-#         "user": current_user
-#     }
+@router.put("/{user_id}", response_model=UserResponse)
+def update_user(
+    user_id: int,
+    user_data: UserUpdate,
+    db: Session = Depends(get_db)
+):
+    return user_service.update_user(db, user_id, user_data)
+
+@router.delete("/{user_id}")
+def delete_user(
+    user_id: int,
+    db: Session = Depends(get_db)
+):
+    return user_service.delete_user(db, user_id)
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,9 +1,26 @@
+import logging
+from fastapi import HTTPException, status
 from app.schemas.user import UserCreate
 from sqlalchemy.orm import Session
 from app.models.user import User
-from app.schemas.user import UserCreate
+from app.schemas.user import UserCreate, UserUpdate
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+
+logger = logging.getLogger(__name__)
 
 def create_user(user_data: UserCreate, db: Session):
+    
+    existing_user = db.scalar(
+        select(User).where(User.email == user_data.email)
+    )
+    
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Email already exists"
+        )
+    
     user = User(
         name = user_data.name,
         email = user_data.email,
@@ -16,3 +33,53 @@ def create_user(user_data: UserCreate, db: Session):
     db.refresh(user)
     
     return user
+
+
+def get_users(db: Session):
+    return db.scalars(
+        select(User)
+    ).all()
+    
+def get_user(db:Session, user_id: int):
+    user = db.get(User, user_id)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+        
+    return user
+
+
+def update_user(db: Session, user_id: int, user_data: UserUpdate):
+    user = get_user(db, user_id)
+    
+    update_data = user_data.model_dump(exclude_unset=True)
+    
+    for field, value in update_data.items():
+        setattr(user, field, value)
+    
+    try:
+        db.commit()
+        db.refresh(user)
+    except IntegrityError as e:
+        logger.error(f"Database integrity error on user_id: {user_id} : {e}")
+        logger.error(f"DB error => {e.orig}")
+        
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Email already registered"
+        )
+        
+    return user
+
+
+def delete_user(db: Session, user_id: int):
+    
+    user = get_user(db, user_id)
+    db.delete(user)
+    db.commit()
+    
+    return {
+        "message": "user deleted successfully"
+    }
